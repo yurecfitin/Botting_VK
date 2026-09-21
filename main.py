ec313c90dd3ade29192955b823f3dfc4c08201dd
yurecfitin <138714397+yurecfitin@users.noreply.github.com>
2026-09-21 16:11:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import asyncio
 import os
 from telebot.async_telebot import AsyncTeleBot
+from telebot import types
 
 TOKEN = "7842967942:AAHrHAIabZNZBWEnBfEHzxjhLKR5ar4e994"
 
 bot = AsyncTeleBot(token=TOKEN)
-
 # Обработка команд /start и /help
 @bot.message_handler(commands=['start', 'help'])
 async def send_welcome(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton('Начать диалог', callback_data='start_dialog')
+    markup.add(btn)
     text = "Здравствуйте, {}!\n
     "Приглашаем вас принять участие в {}, которое состоится {} в {}.".format(
         message.from_user.first_name, "ДогиСтаил", 18, 30
     )
-    await bot.reply_to(message, text)
+    await bot.send_message(message, text, reply_markup=markup)
 
 # Обработка всех остальных текстовых сообщений
 @bot.message_handler(func=lambda message: True)
