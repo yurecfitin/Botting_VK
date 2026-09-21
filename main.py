daefc91b057d338dfc3cab1aa4b41bc6477d60ec
yurecfitin <138714397+yurecfitin@users.noreply.github.com>
2026-09-21 16:00:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot = AsyncTeleBot(token=TOKEN)
 # Обработка команд /start и /help
 @bot.message_handler(commands=['start', 'help'])
 async def send_welcome(message):
-    text = 'Здравствуйте, {}!\nПриглашаем вас принять участие в {}, которое состоится {} в {}.'.format(message.user_name, "ДогиСтаил", 18, 30)
+    text = 'Здравствуйте, {}!\nПриглашаем вас принять участие в {}, которое состоится {} в {}.'.format(message.from_user.name, "ДогиСтаил", 18, 30)
     await bot.reply_to(message, text)
 
 # Обработка всех остальных текстовых сообщений
